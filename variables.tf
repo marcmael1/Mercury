0bd89b2ecfd42f5996f6019322893c0d8651d8dd
marcmael1 <marctchouanche@gmail.com>
2022-11-10 18:56:29 -0500

    Added terraform.tfvars file, variables file and modified vpc file

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "env_code" {
+
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_cidr" {
+  default = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "private_cidr" {
+  default = ["10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "route_cidr" {
+  default = "0.0.0.0/0"
+}
