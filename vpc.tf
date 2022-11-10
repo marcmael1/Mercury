9a90698e3ae172e1024b16e762b8fe9455795389
marcmael1 <marctchouanche@gmail.com>
2022-11-09 21:25:32 -0500

    add count and meta-argument

--- vpc.tf
+++ vpc.tf
@@ -7,43 +7,29 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public_subnet1" {
+locals {
+  public_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
+}
+
+resource "aws_subnet" "public_subnet" {
+  count = length(local.public_cidr)
+
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+  cidr_block        = local.public_cidr[count.index]
 
   tags = {
-    Name = "public-subnet1"
+    Name = "public-subnet ${count.index}"
   }
 }
 
-resource "aws_subnet" "public_subnet2" {
+resource "aws_subnet" "private_subnet" {
+  count = length(local.private_cidr)
+
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
-
+  cidr_block        = local.private_cidr[count.index]
   tags = {
-    Name = "public-subnet2"
-  }
-}
-
-resource "aws_subnet" "private_subnet1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "private-subnet1"
-  }
-}
-
-resource "aws_subnet" "private_subnet2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "private-subnet2"
+    Name = "private-subnet${count.index}"
   }
 }
 
@@ -68,84 +54,53 @@ resource "aws_route_table" "public_rtb" {
   }
 }
 
-resource "aws_route_table_association" "public_association1" {
-  subnet_id      = aws_subnet.public_subnet1.id
+resource "aws_route_table_association" "public_association" {
+  count = length(local.public_cidr)
+
+  subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public_rtb.id
 }
 
-resource "aws_route_table_association" "public_association2" {
-  subnet_id      = aws_subnet.public_subnet2.id
-  route_table_id = aws_route_table.public_rtb.id
-}
+resource "aws_route_table" "private_rtb" {
+  count = length(local.private_cidr)
 
-resource "aws_route_table" "private_rtb1" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ngw1.id
+    nat_gateway_id = aws_nat_gateway.ngw[count.index].id
   }
 
   tags = {
-    Name = "private_rtb1"
+    Name = "private_rtb ${count.index}"
   }
 }
 
-resource "aws_route_table_association" "private_association1" {
-  subnet_id      = aws_subnet.private_subnet1.id
-  route_table_id = aws_route_table.private_rtb1.id
+resource "aws_route_table_association" "private_association" {
+  count = length(local.private_cidr)
+
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_rtb[count.index].id
 }
 
-resource "aws_route_table" "private_rtb2" {
-  vpc_id = aws_vpc.main.id
+resource "aws_eip" "eip-nat" {
+  count = length(local.public_cidr)
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ngw2.id
-  }
-
-  tags = {
-    Name = "private_rtb2"
-  }
-}
-
-resource "aws_route_table_association" "private_association2" {
-  subnet_id      = aws_subnet.private_subnet2.id
-  route_table_id = aws_route_table.private_rtb2.id
-}
-
-resource "aws_eip" "eip1" {
   vpc = true
 
   tags = {
-    "Name" = "ngw1-eip"
+    "Name" = "eip-nat ${count.index}"
   }
 }
 
-resource "aws_nat_gateway" "ngw1" {
-  allocation_id = aws_eip.eip1.id
-  subnet_id     = aws_subnet.public_subnet1.id
+resource "aws_nat_gateway" "ngw" {
+  count = length(local.public_cidr)
+
+  allocation_id = aws_eip.eip-nat[count.index].id
+  subnet_id     = aws_subnet.public_subnet[count.index].id
   depends_on    = [aws_internet_gateway.igw]
 
   tags = {
-    Name = "ngw1"
-  }
-}
-
-resource "aws_eip" "eip2" {
-  vpc = true
-
-  tags = {
-    "Name" = "ngw2-eip"
-  }
-}
-
-resource "aws_nat_gateway" "ngw2" {
-  allocation_id = aws_eip.eip2.id
-  subnet_id     = aws_subnet.public_subnet2.id
-  depends_on    = [aws_internet_gateway.igw]
-
-  tags = {
-    Name = "ngw2"
+    Name = "ngw ${count.index}"
   }
 }
